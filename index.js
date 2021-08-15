2fc4a130beecdcc6df04000ceb55b216ca5f2888
Ayush Patel <patel.ayush08@gmail.com>
2021-08-15 14:32:55 +0900

    Created base server

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const http = require('http')
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const hostname = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+
+server.listen(port, hostname, () => {
+console.log('Server running at http://${hostname}:${port}/')
+}
