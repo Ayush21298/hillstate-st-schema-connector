8c0dc499ad58f2f9a3c8d7cae2fa00ff0c589d8d
Ayush Patel <patel.ayush08@gmail.com>
2021-08-17 18:14:17 +0900

    Added Eslint and some response format

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,34 +1,38 @@
 const axios = require('axios')
 const updateDotenv = require('update-dotenv')
 
-const auth_url = 'https://www2.hthomeservice.com'
-const login_url = auth_url + '/proxy/htservice/oauth/token'
+const authUrl = 'https://www2.hthomeservice.com'
+const loginUrl = authUrl + '/proxy/htservice/oauth/token'
 
 const username = process.env.HILLSTATE_USERNAME || 'User'
 const password = process.env.HILLSTATE_PASSWORD || 'Password'
 
 module.exports = {
-  login: function(req, res) {
-    axios.post(login_url, {
+  login: function (req, res) {
+    axios.post(loginUrl, {
       username: username,
       password: password
     })
-    .then(function(response) {
-      console.log(response);
-      updateDotenv({
-        ACCESS_TOKEN: response.data.access_token
-      }).then((newEnv) => console.log('Updated ACCESS_TOKEN!', newEnv))
-      if(res) return res.status(200).json({
-        message: "Login Sucessful",
-        data: response.data
+      .then(function (response) {
+        console.log(response)
+        updateDotenv({
+          ACCESS_TOKEN: response.data.access_token
+        }).then((newEnv) => console.log('Updated ACCESS_TOKEN!', newEnv))
+        if (res) {
+          return res.status(200).json({
+            message: 'Login Sucessful',
+            data: response.data
+          })
+        }
       })
-    })
-    .catch(function(error) {
-      console.log(error);
-      if(res) return res.status(500).json({
-        message: "Can't Login",
-        error: error
+      .catch(function (error) {
+        console.log(error)
+        if (res) {
+          return res.status(500).json({
+            message: "Can't Login",
+            error: error
+          })
+        }
       })
-    })
   }
 }
--- controllers/device.js
+++ controllers/device.js
@@ -2,40 +2,60 @@ const axios = require('axios')
 
 const authController = require('./auth')
 
-const base_url = 'https://hillstategwanggyo.hthomeservice.com'
-const status_url = base_url + '/proxy/core/device/status/all/get'
-const command_url = base_url + '/proxy/core/homepage/device/command'
+const baseUrl = 'https://hillstategwanggyo.hthomeservice.com'
+const statusUrl = baseUrl + '/proxy/core/device/status/all/get'
+const commandUrl = baseUrl + '/proxy/core/homepage/device/command'
 
 const houseLoc = require('../house/location.json')
+const devices = require('../house/devices.json')
 
 module.exports = {
-  getStatus: function(req, res) {
-    axios.post(status_url, houseLoc, {
+  getStatus: function (req, res) {
+    axios.post(statusUrl, houseLoc, {
       params: {
         access_token: process.env.ACCESS_TOKEN || 'access_token'
-      },
-    })
-    .then(function(response) {
-      console.log(response);
-      return res.status(200).json({
-        message: "Device status received",
-        data: response.data
-      })
-    })
-    .catch(function(error) {
-      console.log(error);
-      if(error.response.statusText == 'Unauthorized') {
-        authController.login()
-        return module.exports.getStatus(req, res)
-      } else {
-        return res.status(500).json({
-          message: "Error getting device status",
-          error: error
-        })
       }
     })
+      .then(function (response) {
+        console.log(response)
+        const out = {
+          headers: {
+            schema: 'st-schema',
+            version: '1.0',
+            interactionType: 'discoveryResponse',
+            requestId: 'abc-123-456'
+          },
+          devices: [{
+            externalDeviceId: 'fake-c2c-dimmer',
+            friendlyName: 'Virtual Switch',
+            deviceHandlerType: 'c2c-dimmer',
+            manufacturerInfo: {
+              manufacturerName: 'SmartThings',
+              modelName: 'Virtual SmartThings device',
+              hwVersion: 'v1 US bulb',
+              swVersion: '23.123.231'
+            }
+          }]
+        }
+        return res.status(200).json({
+          message: 'Device status received',
+          data: response.data.deviceStatusAllList[0].deviceList
+        })
+      })
+      .catch(function (error) {
+        console.log(error)
+        if (error.response.statusText === 'Unauthorized') {
+          authController.login()
+          return module.exports.getStatus(req, res)
+        } else {
+          return res.status(500).json({
+            message: 'Error getting device status',
+            error: error
+          })
+        }
+      })
   },
 
-  setStatus: function(req, res) {
+  setStatus: function (req, res) {
   }
 }
